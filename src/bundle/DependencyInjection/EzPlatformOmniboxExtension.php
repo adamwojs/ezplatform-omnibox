16788ddb8b708c0ad6d0491b7ded5a72fda506af
Adam Wójs <adam@wojs.pl>
2020-11-08 18:37:00 +0100

    Added omnibox input to top navigation

--- src/bundle/DependencyInjection/EzPlatformOmniboxExtension.php
+++ src/bundle/DependencyInjection/EzPlatformOmniboxExtension.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace AdamWojs\EzPlatformOmniboxBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-final class EzPlatformOmniboxExtension extends Extension
+final class EzPlatformOmniboxExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -19,5 +22,19 @@ final class EzPlatformOmniboxExtension extends Extension
         );
 
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependEzDesignConfiguration($container);
+    }
+
+    private function prependEzDesignConfiguration(ContainerBuilder $container): void
+    {
+        $eZDesignConfigFile = __DIR__ . '/../Resources/config/ezdesign.yaml';
+        $config = Yaml::parseFile($eZDesignConfigFile);
+
+        $container->prependExtensionConfig('ezdesign', $config['ezdesign']);
+        $container->addResource(new FileResource($eZDesignConfigFile));
     }
 }
