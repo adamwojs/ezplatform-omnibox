fd4a895cb1f9528466dcccbeca279d2ed46d9530
Adam Wójs <adam@wojs.pl>
2020-11-28 21:36:29 +0100

    Initial Web Speech API support

--- src/bundle/Resources/public/js/ezomnibox-init.js
+++ src/bundle/Resources/public/js/ezomnibox-init.js
@@ -1,17 +1,49 @@
 ((global, doc, eZ) => {
+    const attachSpeechRecognizer = (input, onResult) => {
+        const SpeechRecognition = global.SpeechRecognition || global.webkitSpeechRecognition;
+
+        if (SpeechRecognition === undefined) {
+            // Web Speech API is undefined
+            return ;
+        }
+
+        const recognizer = new SpeechRecognition();
+        recognizer.continuous = true;
+        recognizer.interimResults = false
+        recognizer.onresult = onResult;
+
+        input.addEventListener('blur', (e) => {
+            recognizer.stop()
+        });
+
+        input.addEventListener('focus', (e) => {
+            recognizer.start();
+        });
+    }
+
     const form = doc.getElementById('omnibox');
     const input = form.querySelector('input');
 
     doc.addEventListener('DOMContentLoaded', () => {
-        const providers = eZ.omnibox.getProviders();
-        const onAutocompleteSelected = (e, suggestion, dataset) => {
+        const search = autocomplete(input, {
+            openOnFocus: true,
+            hint: true
+        }, eZ.omnibox.getProviders());
+
+        search.on('autocomplete:selected', (e, suggestion, dataset) => {
             if (suggestion && suggestion.actionUrl.length !== 0) {
                 global.location = suggestion.actionUrl;
             }
-        };
+        });
 
-        autocomplete(input, {hint: true}, providers)
-            .on('autocomplete:selected', onAutocompleteSelected);
+        attachSpeechRecognizer(input, (event) => {
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                const result = event.results[i];
+                if (result.isFinal) {
+                    search.autocomplete.setVal(search.autocomplete.getVal() +  ' ' + result[0].transcript);
+                }
+            }
+        });
     });
 
     doc.addEventListener('keydown', (e) => {
